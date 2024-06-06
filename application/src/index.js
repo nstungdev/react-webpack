e74ae77e058f05c5db33fb781ac1e5a0b442f48c
Nguyen-Son-Tung <tung.sonnguyen@infodation.vn>
2024-06-06 15:20:18 +0700

    finish create

---
+++ application/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+function App() {
+    return (
+        <>
+            <h1>Welcome you!</h1>
+            <h1>Welcome you!</h1>
+        </>
+    );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
